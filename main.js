d1c3b352b230181edea1817744cb841a38e4a84f
radishengine <radishengine@gmail.com>
2016-12-22 17:07:26 +0000

    Update main.js

--- main.js
+++ main.js
@@ -64,7 +64,151 @@ gl.useProgram(program);
 var viewport = gl.getParameter(gl.VIEWPORT);
 var width = viewport[2], height = viewport[3];
 
-var vertices = shape;
+var startx = shape[0], starty = shape[1];
+var vertices = [startx, starty];
+var curve_recursion_limit = 32;
+var curve_distance_epsilon = 1e-30;
+var curve_collinearity_epsilon = 1e-30;
+var curve_angle_tolerance_epsilon = 0.01;
+function recursive_bezier(x1,y1, x2,y2, x3,y3, x4,y4, level) {
+  if (level > curve_recursion_limit) {
+    return;
+  }
+
+  // midpoints
+  var x12 = (x1 + x2) / 2, y12 = (y1 + y2) / 2,
+    x23   = (x2 + x3) / 2, y23   = (y2 + y3) / 2,
+    x34   = (x3 + x4) / 2, y34   = (y3 + y4) / 2,
+    x123  = (x12 + x23) / 2, y123  = (y12 + y23) / 2,
+    x234  = (x23 + x34) / 2, y234  = (y23 + y34) / 2,
+    x1234 = (x123 + x234) / 2, y1234 = (y123 + y234) / 2;
+
+  // always subdivide the first time
+  if (level > 0) {
+    // Try to approximate the full cubic curve by a single straight line
+    //------------------
+    var dx = x4-x1, dy = y4-y1;
+
+    var d2 = Math.abs(((x2 - x4) * dy - (y2 - y4) * dx));
+    var d3 = Math.abs(((x3 - x4) * dy - (y3 - y4) * dx));
+
+    var da1, da2;
+
+    if (d2 > curve_collinearity_epsilon && d3 > curve_collinearity_epsilon) { 
+      // Regular care
+      //-----------------
+      if ((d2 + d3)*(d2 + d3) <= m_distance_tolerance * (dx*dx + dy*dy)) {
+        // If the curvature doesn't exceed the distance_tolerance value
+        // we tend to finish subdivisions.
+        //----------------------
+        if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+          vertices.push(x1234, y1234);
+          return;
+        }
+
+        // Angle & Cusp Condition
+        //----------------------
+        var a23 = Math.atan2(y3 - y2, x3 - x2);
+        da1 = Math.abs(a23 - Math.atan2(y2 - y1, x2 - x1));
+        da2 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - a23);
+        if (da1 >= Math.pi) da1 = 2*Math.pi - da1;
+        if (da2 >= Math.pi) da2 = 2*Math.pi - da2;
+
+        if (da1 + da2 < m_angle_tolerance) {
+          // Finally we can stop the recursion
+          //----------------------
+          vertices.push(x1234, y1234);
+          return;
+        }
+
+        if (m_cusp_limit != 0.0) {
+          if (da1 > m_cusp_limit) {
+            vertices.push(x2, y2);
+            return;
+          }
+
+          if (da2 > m_cusp_limit) {
+            vertices.push(x3, y3);
+            return;
+          }
+        }
+      }
+    }
+    else {
+      if (d2 > curve_collinearity_epsilon) {
+          // p1,p3,p4 are collinear, p2 is considerable
+          //----------------------
+          if (d2 * d2 <= m_distance_tolerance * (dx*dx + dy*dy)) {
+              if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+                vertices.push(x1234, y1234);
+                return;
+              }
+
+              // Angle Condition
+              //----------------------
+              da1 = Math.abs(Math.atan2(y3 - y2, x3 - x2) - Math.atan2(y2 - y1, x2 - x1));
+              if (da1 >= Math.pi) da1 = 2*Math.pi - da1;
+
+              if (da1 < m_angle_tolerance) {
+                vertices.push(x2,y2, x3,y3);
+                return;
+              }
+
+              if (m_cusp_limit != 0.0 && da1 > m_cusp_limit) {
+                vertices.push(x2,y2);
+                return;
+              }
+          }
+      }
+      else if (d3 > curve_collinearity_epsilon) {
+        // p1,p2,p4 are collinear, p3 is considerable
+        //----------------------
+        if (d3 * d3 <= m_distance_tolerance * (dx*dx + dy*dy)) {
+          if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+            vertices.push(x1234, y1234);
+            return;
+          }
+
+          // Angle Condition
+          //----------------------
+          da1 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - Math.atan2(y3 - y2, x3 - x2));
+          if (da1 >= pi) da1 = 2*pi - da1;
+
+          if (da1 < m_angle_tolerance) {
+            vertices.push(x2,y2, x3,y3);
+            return;
+          }
+
+          if (m_cusp_limit !== 0.0 && da1 > m_cusp_limit) {
+            vertices.push(x3,y3);
+            return;
+          }
+        }
+      }
+      else {
+        // Collinear case
+        //-----------------
+        dx = x1234 - (x1 + x4) / 2;
+        dy = y1234 - (y1 + y4) / 2;
+        if (dx*dx + dy*dy <= m_distance_tolerance) {
+          vertices.push(x1234, y1234);
+          return;
+        }
+      }
+    }
+  }
+
+  recursive_bezier(x1, y1, x12, y12, x123, y123, x1234, y1234, level + 1);
+  recursive_bezier(x1234, y1234, x234, y234, x34, y34, x4, y4, level + 1);
+}
+for (var i = 2; i < shape.length; i += 6) {
+  var c1x = shape[i], c1y = shape[i+1],
+      c2x = shape[i+2], c2y = shape[i+3],
+      endx = shape[i+4], endy = shape[i+5];
+  recursive_bezier(startx, startx, c1x, c1y, c2x, c2y, endx, endy);
+  startx = endx;
+  startx = endy;
+}
 
 var vertex_buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
