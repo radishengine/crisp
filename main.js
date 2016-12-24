a87a2d633f15b6f721ef248ae9ba2e5a9f627756
radishengine <radishengine@gmail.com>
2016-12-24 16:54:07 +0000

    Update main.js

--- main.js
+++ main.js
@@ -275,8 +275,8 @@ function createGradientTexture(r, g, b, a, r2,g2,b2,a2) {
   var texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 2, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   return texture;
@@ -284,7 +284,7 @@ function createGradientTexture(r, g, b, a, r2,g2,b2,a2) {
 
 gl.uniform1i(gl.getUniformLocation(program, "gradientSampler"), 6);
 gl.activeTexture(gl.TEXTURE6);
-gl.bindTexture(gl.TEXTURE_2D, createGradientTexture(255,0,0,255, 0,255,0,255));
+gl.bindTexture(gl.TEXTURE_2D, createGradientTexture(0,0,0,255, 255,255,255,255));
 
 gl.clearColor(0,0,0, 1);
 
